3ecdb5fd53e931ccfa5c3819f74520aef6b3c093
Wyatt Lipscomb <35582368+WyattPlayz@users.noreply.github.com>
2018-12-02 18:40:30 -0500

    Update updater.lua

--- os/updater.lua
+++ os/updater.lua
@@ -1,7 +1,31 @@
 local updater = {}
 
-local function updater.update()
-  local function
+local function spinner()
+  local function writeAt(text, x, y, color) term.setTextColor(color) term.setCursorPos(x, y) term.write(text) end
+  local function draw(x, y, color) term.setBackgroundColor(color) term.setCursorPos(x, y) term.write(" ") end
+  local function getWidth() local w, h = term.getSize() return w end
+  while true do
+    term.clear()
+    writeAt("Error Code: 0", 1, 1, colors.white)
+    writeAt("Error Message: updater.lua stopped bootup on bios level", 1, 2, colors.white)
+    writeAt("message: updating. +")
+    os.sleep(.25
+    term.clear()
+    writeAt("Error Code: 0", 1, 1, colors.white)
+    writeAt("Error Message: updater.lua stopped bootup on bios level", 1, 2, colors.white)
+    writeAt("message: updating. x")
+    os.sleep(.25)
+  end
+end
+
+function updater.update()
+  local function writeAt(text, x, y, color) term.setTextColor(color) term.setCursorPos(x, y) term.write(text) end
+  local function draw(x, y, color) term.setBackgroundColor(color) term.setCursorPos(x, y) term.write(" ") end
+  local function getWidth() local w, h = term.getSize() return w end
+  
+  term.setBackgroundColor(colors.lightBlue)
+  term.clear()
+  spinner()
 end
 
 return updater
