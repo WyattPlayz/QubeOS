e96367e81c287768853e2b6b092967adfb6bde7f
Wyatt Lipscomb <35582368+WyattPlayz@users.noreply.github.com>
2018-12-02 19:35:13 -0500

    Update login.lua

--- os/login.lua
+++ os/login.lua
@@ -8,7 +8,7 @@ local function writeAt(text, x, y, color) term.setTextColor(color) term.setCurso
 local function draw(x, y, color) term.setBackgroundColor(color) term.setCursorPos(x, y) term.write(" ") end
 local function getWidth() local w, h = term.getSize() return w end
 
-local iop = jsonreader.loadJson("/os/system/iop.json")
+local iop = jsonreader.loadJson("/os/iop.json")
 for _, k, v in pairs(iop) do
   local currentPassword = v
 end
