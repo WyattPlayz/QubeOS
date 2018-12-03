e814d9cd8bdf1bbc11cd091f24f96b88d3ebe76c
Wyatt Lipscomb <35582368+WyattPlayz@users.noreply.github.com>
2018-12-02 19:49:18 -0500

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -6,6 +6,7 @@
   local function writeAt(text, x, y, color) term.setTextColor(color) term.setCursorPos(x, y) term.write(text) end
   local function draw(x, y, color) term.setBackgroundColor(color) term.setCursorPos(x, y) term.write(" ") end
   local function getWidth() local w, h = term.getSize() return w end
+  os.sleep
   while true do
     term.clear()
     writeAt("Error Code: 0", 1, 1, colors.white)
@@ -19,13 +20,12 @@
     os.sleep(.25)
   end
 end
-  
+  local function install()
   term.setBackgroundColor(colors.blue)
   term.clear()
   writeAt('Interruption. Entering BIOS Mode.', 1, 1, colors.white)
   writeAt('Error Code: 0', 1, 2, colors.white)
   writeAt('Error Message: installer.lua has caused a bios interruption.', 1, 3, colors.white)
-  spinner()
   
   local files = http.request('https://raw.githubusercontent.com/WyattPlayz/QubeOS/master/installlocations.txt')
   local event, url, handle = os.pullEvent('http_success')
@@ -41,3 +41,6 @@ end
       handle2:close()
   end
   os.reboot()
+end
+
+parallel.waitForAll(install, spinner)
