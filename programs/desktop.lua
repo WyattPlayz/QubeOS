0b7ae93bfc3668cb506db7471c4e7b887a52406a
Wyatt Lipscomb <35582368+WyattPlayz@users.noreply.github.com>
2019-01-04 16:28:42 -0500

    Update desktop.lua

--- programs/desktop.lua
+++ programs/desktop.lua
@@ -1,5 +1,6 @@
   local function writeAt(text, x, y, color) term.setTextColor(color) term.setCursorPos(x, y) term.write(text) end
   local function draw(x, y, color) term.setBackgroundColor(color) term.setCursorPos(x, y) term.write(" ") end
   local function getWidth() local w, h = term.getSize() return w end
+  local function getHeight() local w, h = term.getSize() return h end
   
   writeAt("
