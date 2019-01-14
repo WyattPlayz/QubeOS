cddf2820d06fa3d2954c67c3c1672d3b73b823d8
Wyatt Lipscomb <35582368+WyattPlayz@users.noreply.github.com>
2019-01-14 11:40:37 -0500

    Update desktop.lua

--- programs/desktop.lua
+++ programs/desktop.lua
@@ -1,6 +1,11 @@
-  local function writeAt(text, x, y, color) term.setTextColor(color) term.setCursorPos(x, y) term.write(text) end
-  local function draw(x, y, color) term.setBackgroundColor(color) term.setCursorPos(x, y) term.write(" ") end
-  local function getWidth() local w, h = term.getSize() return w end
-  local function getHeight() local w, h = term.getSize() return h end
+local function writeAt(text, x, y, color) term.setTextColor(color) term.setCursorPos(x, y) term.write(text) end
+local function draw(x, y, color) term.setBackgroundColor(color) term.setCursorPos(x, y) term.write(" ") end
+local function getWidth() local w, h = term.getSize() return w end
+local function getHeight() local w, h = term.getSize() return h end
+
+local debugmode = false
+
+local function debug(msg) if debugmode then rednet.broadcast(msg, 'debug') end
+
   
-  writeAt("
+if debugmode then writeAt("Debug Mode ON", getWidth()-13, 0, colors.yellow) end
